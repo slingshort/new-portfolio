3d094d1f57a420d9945eb9f62160384e1bef2235
Salina Myat <salinamyat@Salinas-MacBook-Air.local>
2023-06-29 09:50:02 +1000

    projects

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
 import './App.css';
 import Header from './components/Header';
-import { motion } from "framer-motion"
-import profile from './assets/profile-picture.jpg'
-// import resume from "../assets/Salina_resume.pdf"
+import projects from './components/ProjectList';
+import { motion } from "framer-motion";
+import profile from './assets/profile-picture.jpg';
+import {BiMailSend} from "react-icons/bi"
+
 
 function App() {
+
+  const portfolioComponents = projects.map((project) => (
+    <div key={project.id}>
+      <h3>{project.title}</h3>
+      {/* <img
+        alt={project.title}
+        src={project.image}
+      /> */}
+      <p>{project.description}</p>
+      <div className="flex-bottom">
+        <button><a href={project.live}>Live</a></button>
+        <button><a href={project.repo}>Repo</a></button>
+      </div>
+    </div>
+  ))
+
   return (
     <div className='app'>
       <Header />
@@ -16,6 +34,7 @@ function App() {
           <h1 className='text-slate-50'>SLINGSHORT.</h1>
           <h2 className='text-fuchsia-400'> Full stack web-developer</h2>
         </div>
+
         <div className='grid grid-cols-3'>
           <div className='col-span-1 place-content-center p-center'>
             <img src={profile} alt='profile-pic'/>
@@ -23,9 +42,21 @@ function App() {
           <div className='grid col-span-2 place-content-center p-center'>
             <h1 className='text-fuchsia-400 text-6xl'>Hey, I'm Salina</h1>
             <p className='text-slate-50 text-2xl'> I've spent the past year developing my skills as a full-stack developer. Take a look at some of the projects I've worked on below!</p>
-            <p className='text-slate-50 text-2xl'> I'm also currently working as a professional musician. In case you're interested, you can take a look at some of the work I've been involved in with <a className='inline-link' href='https://www.taikoz.com/'>Taikoz</a> or <a className='inline-link' href='https://www.drummerqueens.com/'>Drummer Queens</a> </p>
-            <button className='justify-self-start px-8'>Let's connect!</button>
+            <p className='text-slate-50 text-2xl'> I'm also currently working as a professional musician. In case you're interested, you can take a look at some of the work I've been involved in with <a className='inline-link' href='https://www.taikoz.com/'>Taikoz</a> or <a className='inline-link' href='https://www.drummerqueens.com/'>Drummer Queens</a>.</p>
+            <button className='justify-self-start px-8 my-1'>Let's connect!</button>
           </div>
+        </div>
+
+        <div className='grid place-content-center p-center'>
+          <h1 className='text-slate-50 text-8xl'>Projects.</h1>
+          <p className='text-fuchsia-400 text-2xl'> Here are some projects I've worked on: </p>
+          <div className="flex-row">{portfolioComponents}</div>
+        </div>
+
+        <div className='grid place-content-center p-center'>
+          <h1 className='text-fuchsia-400 text-8xl'>Contact me.</h1>
+          <p className='text-slate-50 text-2xl'>Here's my email if you'd like to reach out! You can also find me on <a className='inline-link' href='https://www.linkedin.com/in/salina-m-a1655111b/'>Linkedin</a> if you prefer to conact me there.</p>
+          <h2 className='text-fuchsia-400 text-2xl px-2'><a className='inline-link' href='mailto:salina.myat@gmail.com'>salina.myat@gmail.com <span><BiMailSend /></span></a></h2>
         </div>
       </motion.div>
     </div>
---
+++ src/components/ProjectList.js
@@ -0,0 +1,42 @@
+const projects = [
+    {
+      id:"1",
+      title: "Bootcamp Bar",
+    //   image: "/images/bootcamp-bar.png",
+      description: "Group MERN stack project",
+      live: "https://bootcamp-bar.herokuapp.com/",
+      repo: "https://github.com/slingshort/Bootcamp_bar_V2.0",
+    },
+    {
+      id:"2",
+      title: "Holiday Calendar Discoverer",
+    //   image: 'holiday-discoverer.png',
+      description: "First group full-stack project",
+      live: "https://kangazero.github.io/holiday-calendar-discoverer/",
+      repo: "https://github.com/slingshort/holiday-calendar-discoverer",
+    },
+    {
+      id:"3",
+      title: "Legacy Portfolio",
+      description: "My very first project!",
+      live: "https://slingshort.github.io/Portfolio/",
+      repo: "https://github.com/slingshort/holiday-calendar-discoverer",
+    },
+    {
+      id:"4",
+      title: "Work Day Scheduler",
+      description: "Work day scheduler using local storage to store persisting data",
+      live: "https://slingshort.github.io/js_workday_scheduler/",
+      repo: "https://github.com/slingshort/js_workday_scheduler",
+    },
+    {
+      id:"5",
+      title: "Weather Dasboard",
+      description: "Weather Dashboard",
+      live: "https://slingshort.github.io/weather-dash/",
+      repo: "https://github.com/slingshort/weather-dash",
+    },
+  
+  ]
+  
+export default projects;
